16493946511c4dca2329e40ff2769cba8362a9dd
JasmienVanGaever <jasmvang@student.arteveldehs.be>
2017-11-06 14:20:15 +0100

    server

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,8 +3,15 @@ var browserSync = require('browser-sync').create();
 var gulpSass    = require('gulp-sass');
 
 //compile sass into CSS
+gulp.task('serve', function(){
+    browerSync.init({
+        server: './app'
+    });
+    gulp.watch('app/scss/*.scss', ['sass']);
+});
+
 gulp.task('sass', function(){
     return gulp.src('app/scss/*.scss')
-    .pipe(sass())
+    .pipe(gulpSass())
     .pipe(gulp.dest('app/css'));
 });
---
+++ dist/README.md
@@ -0,0 +1,5 @@
+App template
+============
+
+version: 1.0
+last modified: 6/11/2017
